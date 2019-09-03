b908d959f6e673a32390a5ce4e3a455a38f464c9
James Graham <james@hoppipolla.co.uk>
2019-09-03 20:08:51 +0100

    Support displaying graph of failure count

--- docs/index.js
+++ docs/index.js
@@ -42,12 +42,51 @@ async function setCount(runIds, id, untriaged) {
     }
 }
 
+async function getRuns() {
+    let resp = await fetch("runs.json");
+    let runs = await resp.json();
+    return runs;
+
+}
+
+async function drawCharts() {
+    let data = await getRuns();
+    var testChartData = new google.visualization.DataTable();
+    testChartData.addColumn('datetime', 'Run Date');
+    testChartData.addColumn('number', 'Fx-only test failures (all)');
+    testChartData.addColumn('number', 'Fx-only test failures (untriaged)');
+
+    var subtestChartData = new google.visualization.DataTable();
+    subtestChartData.addColumn('datetime', 'Run Date');
+    subtestChartData.addColumn('number', 'Fx-only subtest failures (all)');
+    subtestChartData.addColumn('number', 'Fx-only subtest failures (untriaged)');
+
+    for (let run of data.runs) {
+        testChartData.addRow([new Date(run.date), run.all_failures.tests, run.untriaged_failures.tests]);
+        subtestChartData.addRow([new Date(run.date), run.all_failures.subtests, run.untriaged_failures.subtests]);
+    }
+
+    var testChartOptions = {
+        title: 'Fx-only test failures'
+    };
+    var testChart = new google.visualization.LineChart(document.getElementById('test_chart'));
+    testChart.draw(testChartData, testChartOptions);
+
+    var subtestChartOptions = {
+        title: 'Fx-only subtest failures'
+    };
+    var subtestChart = new google.visualization.LineChart(document.getElementById('subtest_chart'));
+    subtestChart.draw(subtestChartData, subtestChartOptions);
+}
+
 async function render() {
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawCharts);
     let runs = await getRuns();
     if (!runs) {
         return;
     }
-    let runIds = runs.map(run => run.id);
+    let runIds = runs.runs[runs.runs.length - 1].run_ids;
     await Promise.all([setCount(runIds, "all", false),
                        setCount(runIds, "untraiged", true)]);
 }
