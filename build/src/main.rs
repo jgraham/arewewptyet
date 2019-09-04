3f90bc24eafe8afc96c6a431dc27dca64c25e50a
James Graham <james@hoppipolla.co.uk>
2019-09-04 14:03:14 +0100

    Run all update processes

--- build/src/main.rs
+++ build/src/main.rs
@@ -6,11 +6,19 @@ mod network;
 use std::process;
 
 fn main() {
-    match failures::update::run() {
-        Ok(()) => {},
-        Err(e) => {
-            println!("{:?}", e);
-            process::exit(1);
+    let failures_update = failures::update::run();
+    let latency_update = latency::update::run();
+    let exit_code = match (failures_update, latency_update) {
+        (Ok(()), Ok(())) => 0,
+        (x, y) => {
+            if let Err(e) = x {
+                println!("Failures update failed:\n{:?}", e);
+            };
+            if let Err(e) = y {
+                println!("Latency update failed:\n{:?}", e);
+            };
+            1
         }
-    }
+    };
+    process::exit(exit_code);
 }
